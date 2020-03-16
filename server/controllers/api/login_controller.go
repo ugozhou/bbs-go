c359cd292ba253b6cecc97e54b53cf3049b2f223
ugozhou <yishaofan@outlook.com>
2020-03-16 23:08:03 +0800

    增加手机验证码接口，邀请码验证接口

--- server/controllers/api/login_controller.go
+++ server/controllers/api/login_controller.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"github.com/goburrow/cache"
 	"github.com/kataras/iris/v12"
 	"github.com/mlogclub/simple"
+	"time"
 
 	"bbs-go/common"
 	"bbs-go/common/github"
@@ -16,25 +18,49 @@ type LoginController struct {
 	Ctx iris.Context
 }
 
+//type RegistForm struct {
+//	phone string	`json:"phone"`
+//	inviateCode  string	`json:"inviateCode"`
+//	password	string	`json:"password"`
+//	rePassword string	`json:"rePassword"`
+//	captchald string	`json:"captchald"`
+//	ret string	`json:"ret"`
+//}
+var phonecache = cache.New(cache.WithMaximumSize(1000),
+	cache.WithExpireAfterAccess(10*time.Second),
+	cache.WithRefreshAfterWrite(90*time.Second))
+
 // 注册
 func (c *LoginController) PostSignup() *simple.JsonResult {
+
+	//var requestData RegistForm
+	////c.Ctx.JSON(&requestData)
+	//error := c.Ctx.ReadJSON(&requestData)
+	//if error != nil {
+	//	return simple.JsonErrorMsg(error.Error())
+	//}
+	//println(requestData.phone)
 	var (
 		//captchaId   = c.Ctx.PostValueTrim("captchaId")
 		//captchaCode = c.Ctx.PostValueTrim("captchaCode")
 		//email       = c.Ctx.PostValueTrim("email")
-		mobile     = c.Ctx.PostValueTrim("phone")
-		inviteCode = c.Ctx.PostValueTrim("inviteCode")
-		//captchaCode    = c.Ctx.PostValueTrim("captchaCode")
-		password   = c.Ctx.PostValueTrim("password")
-		rePassword = c.Ctx.PostValueTrim("rePassword")
+		mobile      = c.Ctx.PostValueTrim("phone")
+		inviteCode  = c.Ctx.PostValueTrim("inviteCode")
+		messageCode = c.Ctx.PostValueTrim("messageCode")
+		password    = c.Ctx.PostValueTrim("password")
+		rePassword  = c.Ctx.PostValueTrim("rePassword")
 	)
 	if !common.IsValidateMobile(mobile) {
 		return simple.JsonError(common.MobileError)
 	}
+	verifyCode, exist := phonecache.GetIfPresent(mobile)
+	if !exist || messageCode != verifyCode {
+		return simple.JsonErrorMsg("验证码错误")
+	}
 	//if !captcha.VerifyString(captchaId, captchaCode) {
 	//	return simple.JsonError(common.CaptchaError)
 	//}
-	user, err := services.UserService.SignUp(mobile, inviteCode, password, rePassword)
+	user, err := services.UserService.SignUp(mobile, inviteCode, password, password)
 	if err != nil {
 		return simple.JsonErrorMsg(err.Error())
 	}
@@ -58,6 +84,18 @@ func (c *LoginController) PostSignin() *simple.JsonResult {
 		return simple.JsonErrorMsg(err.Error())
 	}
 	return c.GenerateLoginResult(user, ref)
+}
+
+func (c *LoginController) GetVerifyinviatecode() *simple.JsonResult {
+	invitecode := c.Ctx.FormValue("invitecode")
+	if len(invitecode) == 0 {
+		return simple.JsonErrorMsg("参数错误")
+	}
+	result := services.UserService.IsInviteCodeExist(invitecode)
+	if !result {
+		return simple.JsonErrorMsg("邀请码不存在")
+	}
+	return simple.JsonSuccess()
 }
 
 // 退出登录
@@ -129,4 +167,15 @@ func (c *LoginController) GenerateLoginResult(user *model.User, ref string) *sim
 		Put("token", token).
 		Put("user", render.BuildUser(user)).
 		Put("ref", ref).JsonResult()
+}
+
+func (c *LoginController) GetSendsms() *simple.JsonResult {
+
+	//captcha.RandomDigits(4)
+	phone := c.Ctx.FormValue("phone")
+	verifycode := common.GetRandomString(4)
+	phonecache.Put(phone, verifycode)
+	return simple.NewEmptyRspBuilder().
+		Put("testverifycode", verifycode).
+		Put("phone", phone).JsonResult()
 }
